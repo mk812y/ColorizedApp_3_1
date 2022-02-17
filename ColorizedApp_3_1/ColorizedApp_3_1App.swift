ae4081b1046891adf928df0e1ab8aae295a130c0
mk812y <mk812y@gmail.com>
2022-02-17 22:14:05 +0300

    Initial Commit

---
+++ ColorizedApp_3_1/ColorizedApp_3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp_3_1App.swift
+//  ColorizedApp_3_1
+//
+//  Created by Михаил Куприянов on 17.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp_3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorizedApp_3_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorizedApp_3_1
+//
+//  Created by Михаил Куприянов on 17.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
